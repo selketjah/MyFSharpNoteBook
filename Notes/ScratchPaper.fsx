6c10a3625b82a23a070122390a6684415f7ad57e
selketjah <gien.verschatse@gmail.com>
2016-01-09 10:30:17 +0100

    clean up code a bit

---
+++ Notes/ScratchPaper.fsx
@@ -0,0 +1,32 @@
+//session 1 - 8/1/2016
+let myList = [ 1 .. 10 ]
+
+let head::tail = myList
+
+let analyze list =
+  match list with
+  | [] -> "empty"
+  | head::tail -> "head + tail"
+
+let rec sumOf total list =
+  match list with
+  | [] -> total
+  | hd::tl ->
+    let total' = total + hd
+    sumOf total' tl
+
+sumOf 0 myList
+
+let rec take n list =
+  if n = 1 then
+    let hd::tl = list
+    hd
+  else
+    let hd::tl = list
+    take (n-1) tl
+
+take 3 myList
+
+//TODO: check out reduce vs fold
+[1..3] |> Seq.reduce (*)
+[] |> Seq.fold (*) 1

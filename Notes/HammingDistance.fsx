df32e9ca89b72e8a8f958b2878a312e8675af3b1
selketjah <gien.verschatse@gmail.com>
2016-02-08 12:55:03 +0100

    hamming distance

---
+++ Notes/HammingDistance.fsx
@@ -0,0 +1,16 @@
+﻿let hammingDistance (one:string) (two:string) =
+  let toCharArray (word:string) = List.ofSeq word |> List.toArray
+
+  let a = toCharArray one
+  let b = toCharArray two
+
+  let zipped =
+    (a, b)
+    ||> Array.zip
+
+  if one.Length <> two.Length then
+    failwith "words must have same length"
+  else
+    zipped
+    |> Array.filter(fun (a,b) -> (a <> b))
+    |> Array.length

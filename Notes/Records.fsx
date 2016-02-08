f3a6056fd7d2ed6f42caecf34ac5245ee4025ab9
selketjah <gien.verschatse@gmail.com>
2016-02-08 14:05:44 +0100

    intro to types

--- Notes/Records.fsx
+++ Notes/Records.fsx
@@ -1,1 +1,20 @@
-﻿
+﻿type Suit =
+  | Spade
+  | Heart
+  | Diamond
+  | Club
+
+type Rank =
+  | Seven
+  | Eight
+  | Nine
+  | Jack of int
+  | Queen of int
+  | King of int
+  | Ace of int
+  | Ten of int
+
+type Card = Suit * Rank
+
+let sevenSpades = (Suit.Spade, Rank.Seven)
+let jackHearts = (Suit.Heart, Rank.Jack(1))

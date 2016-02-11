f6c17387d13070107ce42eed2e09a0c8d585b947
selketjah <gien.verschatse@gmail.com>
2016-02-11 20:21:37 +0100

    refactor first impl, add sec impl with fold

--- Notes/HammingDistance.fsx
+++ Notes/HammingDistance.fsx
@@ -1,16 +1,20 @@
 ﻿let hammingDistance (one:string) (two:string) =
-  let toCharArray (word:string) = List.ofSeq word |> List.toArray
-
-  let a = toCharArray one
-  let b = toCharArray two
-
-  let zipped =
-    (a, b)
-    ||> Array.zip
-
   if one.Length <> two.Length then
     failwith "words must have same length"
   else
-    zipped
-    |> Array.filter(fun (a,b) -> (a <> b))
-    |> Array.length
+    (one, two)
+    ||> Seq.zip
+    |> Seq.filter(fun (a,b) -> (a <> b))
+    |> Seq.length
+
+
+let hammingDistance2 (one:string) (two:string) =
+  if one.Length <> two.Length then
+    failwith "words must have same length"
+  else
+    (one, two)
+    ||> Seq.zip
+    |> Seq.fold (fun count (a,b) -> 
+      if a=b 
+      then count 
+      else count + 1) 0

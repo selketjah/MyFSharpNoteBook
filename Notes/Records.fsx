dbe1e3b828d1f6616e57de1c6cb741aa4c2a8bb7
selketjah <gien.verschatse@gmail.com>
2016-02-11 20:22:59 +0100

    added some notes

--- Notes/Records.fsx
+++ Notes/Records.fsx
@@ -14,7 +14,20 @@ type Rank =
   | Ace of int
   | Ten of int
 
-type Card = Suit * Rank
+(*let valueOf (card:Rank) =
+  match card with
+  | Seven
+  | Eight
+  | Nine -> 0
+  | King -> 123
+  | _ -> 0
+
+let winnerOf (card1,card2) =
+  match (card1,card2) with
+  | (Seven,_) -> card2
+*)
+
+type Card = Card of Suit * Rank
 
 let sevenSpades = (Suit.Spade, Rank.Seven)
 let jackHearts = (Suit.Heart, Rank.Jack(1))

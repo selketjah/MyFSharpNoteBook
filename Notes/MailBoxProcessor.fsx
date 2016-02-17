9264343c90fb69c94557285c819bb950519d02ef
selketjah <gien.verschatse@gmail.com>
2016-02-17 14:56:02 +0100

    mailprocessor example

---
+++ Notes/MailBoxProcessor.fsx
@@ -0,0 +1,28 @@
+﻿type countMsg =
+    | Die
+    | Incr of int
+    | Fetch of AsyncReplyChannel<int>
+    
+type counter() =
+    let innerCounter =
+        MailboxProcessor.Start(fun inbox ->
+            let rec loop n =
+                async { let! msg = inbox.Receive()
+                        match msg with
+                        | Die -> return ()
+                        | Incr x -> return! loop(n + x)
+                        | Fetch(reply) ->
+                            reply.Reply(n);
+                            return! loop n }
+            loop 0)
+            
+    member this.Incr(x) = innerCounter.Post(Incr x)
+    member this.Fetch() = innerCounter.PostAndReply((fun reply -> Fetch(reply)), timeout = 2000)
+    member this.Die() = innerCounter.Post(Die)
+
+
+let c = new counter()
+c.Incr(7);;
+c.Fetch();;
+c.Incr(12);;
+c.Fetch();;

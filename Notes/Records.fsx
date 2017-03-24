bd96c6270ad6e5350605f152e46766c8be163e82
Gien Verschatse <gien@slotsgames.be>
2017-03-24 10:18:40 +0100

    some updates

--- Notes/Records.fsx
+++ Notes/Records.fsx
@@ -35,3 +35,26 @@ let winnerOf (card1,card2) =
 
 let sevenSpades = (Suit.Spade, Rank.Seven)
 let jackHearts = (Suit.Heart, Rank.Jack)
+
+open System
+
+type Result =
+    | Foo of int
+    | Bar of string
+
+let data = [ Foo(1); Bar("A"); Foo(2)]
+
+let myArray = [|2;1;4;3|]
+
+let arraySortD (x:'a array) = 
+   for j = 2 to x.Length - 1 do
+       let key = x.[j]
+       let mutable i = j - 1
+       while i > 0 && x.[i] > key do
+           x.[i+1] <- x.[i]
+           i <- i-1
+           x.[i+1] <- key
+
+
+printfn "%A" (arraySortD myArray)
+
